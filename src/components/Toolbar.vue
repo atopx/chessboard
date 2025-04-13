a8fc70fe84e546dbb9e7bc600da5fb74ef5d119d
atopx <3940422@qq.com>
2025-04-14 02:24:14 +0800

    fix tauri core

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { invoke } from '@tauri-apps/api';
+import { invoke } from "@tauri-apps/api/core"
 import { NButton, NCard, NFlex, NForm, NFormItem, NInputNumber, NSelect } from 'naive-ui';
 import { onMounted, ref } from 'vue';
 
